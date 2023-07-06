7ab7a6fa947d48e4e43ccd3fd57373d5b9587530
Sergiiweb <sergii.web@gmail.com>
2023-07-06 13:41:35 +0300

    hw2_task1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="wrapper">
+      <div className={"header"}>HW2 Task1</div>
 
 
     </div>
@@ -10,3 +11,7 @@ function App() {
 }
 
 export default App;
+
+// з jsonplaceholder отримати всі пости в компоненту Posts.js
+// відобразити кожного інформацію (id,title) з кожного поста (компонента Post)
+// Зробити кнопку вибора поста, при натисканні на яку в Posts.js ви покажете детальну інфомацію про пост(всю інфу)
