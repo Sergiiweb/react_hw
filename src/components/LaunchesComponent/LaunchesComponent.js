d52e22acd22387d5f70d785eb139a6d0d8bfa369
Sergiiweb <sergii.web@gmail.com>
2023-07-06 23:44:26 +0300

    hw2_task2

--- src/components/LaunchesComponent/LaunchesComponent.js
+++ src/components/LaunchesComponent/LaunchesComponent.js
@@ -2,13 +2,13 @@ import React from 'react';
 import style from './LaunchesComponent.module.css';
 
 const LaunchesComponent = (props) => {
-    const {launche} = props;
+    const {launch} = props;
     return (
 
         <div className={style.container}>
-            <div>mission_name: {launche.mission_name}</div>
-            <div>launch_year: {launche.launch_year}</div>
-            <div><img src={launche.links.mission_patch_small} alt={launche.mission_name}/></div>
+            <div>mission_name: {launch.mission_name}</div>
+            <div>launch_year: {launch.launch_year}</div>
+            <div><img src={launch.links.mission_patch_small} alt={launch.mission_name}/></div>
 
         </div>
     );
--- src/components/LaunchesContainer/LaunchesContainer.js
+++ src/components/LaunchesContainer/LaunchesContainer.js
@@ -8,7 +8,10 @@ const LaunchesContainer = () => {
     useEffect(() => {
         fetch('https://api.spacexdata.com/v3/launches/')
             .then((response) => response.json())
-            .then((launches) => setLaunches(launches))
+            .then((launches) => {
+                const filtredLaunches = launches.filter(value => value.launch_year !== '2020')
+                setLaunches(filtredLaunches)
+            })
             .catch((e) => console.log('error'))
     }, [])
 
@@ -16,11 +19,11 @@ const LaunchesContainer = () => {
     return (
         <>
             <div className={"header"}>HW2 Task2</div>
-            {launches.map((launche) => {
+            {launches.map((launch) => {
                 return (
                     <LaunchesComponent
-                        key={launche.flight_number + launche.launch_date_unix}
-                        launche={launche}
+                        key={launch.flight_number + launch.launch_date_unix}
+                        launch={launch}
                     />)
             })}
         </>
