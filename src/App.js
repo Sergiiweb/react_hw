7d2e58a5d8b815d708e23448945b9f9e718c1491
Sergiiweb <sergii.web@gmail.com>
2023-07-10 14:02:01 +0300

    hw2_task3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import {Posts} from "./components/Posts/Posts";
+import {Users} from "./components/Users/Users";
+import {useState} from "react";
+import {UserPosts} from "./components/Users/UserPosts/UserPosts";
+
 
 function App() {
+    const [userId, setUserId] = useState(null);
     return (
         <div className="wrapper">
-            <Posts/>
+            {/*<Posts/>*/}
+            <Users setUserId={setUserId}/>
+            {userId && <UserPosts userId={userId}/>}
         </div>
     );
 }
---
+++ src/components/Users/User/User.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const User = ({user, setUserId}) => {
+    const {id, name, username, email} = user;
+    return (
+        <div>
+            <div>id: {id}</div>
+            <div>name: {name}</div>
+            <div>username: {username}</div>
+            <div>email: {email}</div>
+            <button onClick={()=>setUserId(id)}>Get Posts</button>
+            <hr/>
+        </div>
+    );
+};
+
+export {User};
