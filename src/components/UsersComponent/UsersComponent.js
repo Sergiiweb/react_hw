6ebe879fd32176093c99b848046888abd6cd6732
Sergiiweb <sergii.web@gmail.com>
2023-07-11 13:50:00 +0300

    hw3_task2

--- src/components/UsersComponent/UsersComponent.js
+++ src/components/UsersComponent/UsersComponent.js
@@ -3,15 +3,16 @@ import {UserComponent} from "./UserComponent/UserComponent";
 
 const UsersComponent = ({isShow, setUserId, setIsShow}) => {
     const [users, setUsers] = useState([]);
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(value => value.json())
             .then(value => setUsers(value))
-    },[])
+    }, [])
     return (
         <div className={'wrap'}>
-            {users.map((user)=><UserComponent key={user.id} user={user} setUserId={setUserId} setIsShow={setIsShow} isShow={isShow}/>)}
+            {users.map((user) => <UserComponent key={user.id} user={user} setUserId={setUserId} setIsShow={setIsShow}
+                                                isShow={isShow}/>)}
         </div>
     );
 };
