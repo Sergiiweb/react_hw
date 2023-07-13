e202c90bcd8caed6505c2f67971f876efc85ede7
Sergiiweb <sergii.web@gmail.com>
2023-07-13 09:45:56 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
-import {UsersComponent} from "./components/UsersComponent/UsersComponent";
-import {useState} from "react";
-import {UserPosts} from "./components/UserPosts/UserPosts";
 
 function App() {
-    const [userId, setUserId] = useState(null);
-    const [isShow, setIsShow] = useState(false);
+
 
     return (
         <div className="wrapper">
-            <UsersComponent setUserId={setUserId} setIsShow={setIsShow} isShow={isShow}/>
-            {userId && isShow && <UserPosts userId={userId}/>}
+
         </div>
     );
 }
