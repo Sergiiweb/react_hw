6c515f5cb33a11f00277b26f3267ed30daa8668a
Sergiiweb <sergii.web@gmail.com>
2023-07-13 12:34:13 +0300

    hw4 task1

--- src/components/CreateUser/CreateUser.js
+++ src/components/CreateUser/CreateUser.js
@@ -1,18 +1,36 @@
 import styles from './CreateUser.module.css'
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 const CreateUser = () => {
-    const [name, setName] = useState('');
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
+    // const [name, setName] = useState('');
+    // const [username, setUsername] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [phone, setPhone] = useState('');
 
-    useEffect(() => {
-        console.log(name, username, email, phone);
-    }, [name, username, email, phone]);
+    // useEffect(() => {
+    //     console.log(name, username, email, phone);
+    // }, [name, username, email, phone]);
+    const nameRef = useRef('');
+    const usernameRef = useRef('');
+    const emailRef = useRef('');
+    const phoneRef = useRef('');
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(name, username, email, phone);
+        console.log(nameRef.current.value, usernameRef.current.value, emailRef.current.value, phoneRef.current.value);
+        fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: nameRef.current.value,
+                username: usernameRef.current.value,
+                email: emailRef.current.value,
+                phone: phoneRef.current.value,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
     }
 
     return (
@@ -22,23 +40,27 @@ const CreateUser = () => {
             <form className={styles.form} onSubmit={handleSubmit}>
                 <label>Name:
                     <br/>
-                    <input type="text" onChange={(event) => setName(event.target.value)} value={name}
-                           placeholder={'name'}/>
+                    <input type="text" ref={nameRef} placeholder={'name'}/>
+                    {/*<input type="text" onChange={(event) => setName(event.target.value)} value={name}*/}
+                    {/*       placeholder={'name'}/>*/}
                 </label>
                 <label>UserName:
                     <br/>
-                    <input type="text" onChange={(event) => setUsername(event.target.value)} value={username}
-                           placeholder={'username'}/>
+                    <input type="text" ref={usernameRef} placeholder={'username'}/>
+                    {/*<input type="text" onChange={(event) => setUsername(event.target.value)} value={username}*/}
+                    {/*       placeholder={'username'}/>*/}
                 </label>
                 <label>Email:
                     <br/>
-                    <input type="email" onChange={(event) => setEmail(event.target.value)} value={email}
-                           placeholder={'email'}/>
+                    <input type="email" ref={emailRef} placeholder={'email'}/>
+                    {/*<input type="email" onChange={(event) => setEmail(event.target.value)} value={email}*/}
+                    {/*       placeholder={'email'}/>*/}
                 </label>
                 <label>Phone:
                     <br/>
-                    <input type="text" onChange={(event) => setPhone(event.target.value)} value={phone}
-                           placeholder={'phone'}/>
+                    <input type="text" ref={phoneRef} placeholder={'phone'}/>
+                    {/*<input type="text" onChange={(event) => setPhone(event.target.value)} value={phone}*/}
+                    {/*       placeholder={'phone'}/>*/}
                 </label>
                 <input type="submit" value={'create'} className={styles.submitB}/>
             </form>
