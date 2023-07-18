5f3d72f0a3ac6b6ff73a26725e508fb970c4b1a4
Sergiiweb <sergii.web@gmail.com>
2023-07-18 20:03:57 +0300

    hw5_1

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -10,32 +10,20 @@ export const UserServices = {
         instance.get(Endpoints.USERS)
             .then(value => setUsers(value.data))
     },
-    // getUsers: (setUsers) => {
-    //     fetch(baseUrl + Endpoints.USERS)
-    //         .then(value => value.json())
-    //         .then(value => setUsers(value))
-    // },
     addUser: (user, setUsers) => {
+        const {name, username, email, address: {city}} = user;
+
         instance.post(Endpoints.USERS, {
-            body: JSON.stringify(user)
+            name: name,
+            username: username,
+            email: email,
+            address:{city: city}
         })
             .then(value => {
-                // setUsers(prev => [...prev, value.data.body]);
+                setUsers(prev => [...prev, value.data]);
                 console.log(value.data);
             })
-    },
-    // addUser: (user, setUsers) => {
-    //     fetch(baseUrl + Endpoints.USERS, {
-    //         headers: {'Content-type': 'application/json'},
-    //         method: 'POST',
-    //         body: JSON.stringify(user)
-    //     })
-    //         .then(value => value.json())
-    //         .then(value => {
-    //             setUsers(prev => [...prev, value]);
-    //             console.log(value);
-    //         })
-    // }
+    }
 }
 
 export const CommentServices = {
@@ -43,42 +31,17 @@ export const CommentServices = {
         instance.get(Endpoints.COMMENTS)
             .then(value => setComments(value.data))
     },
-    // getComments: (setComments) => {
-    //     fetch(baseUrl + Endpoints.COMMENTS)
-    //         .then(value => value.json())
-    //         .then(value => setComments(value))
-    // },
     addComment: (comment, setComments) => {
+        const {postId, name, email,body} = comment;
         instance.post(Endpoints.COMMENTS, {
-            body: JSON.stringify(comment)
+            postId: postId,
+            name: name,
+            email: email,
+            body: body
         })
             .then(value => {
                 setComments(prev => [...prev, value.data]);
                 console.log(value.data);
-                console.log(JSON.parse(value.data.body));
             })
-    },
-    // addComment: (comment, setComments) => {
-    //     fetch(baseUrl + Endpoints.COMMENTS, {
-    //         headers: {'Content-type': 'application/json'},
-    //         method: 'POST',
-    //         body: JSON.stringify(comment)
-    //     })
-    //         .then(value => value.json())
-    //         .then(value => {
-    //             setComments(prev => [...prev, value]);
-    //             console.log(value);
-    //         })
-    // },
-    // addComment: (comment, setComments) => {
-    //     axios({
-    //         method: 'post',
-    //         url: baseUrl + Endpoints.COMMENTS,
-    //         data: JSON.stringify(comment)
-    //     })
-    //             .then(value => {
-    //                 setComments(prev => [...prev, value.data]);
-    //                 console.log(value);
-    //     });
-    // },
+    }
 }
